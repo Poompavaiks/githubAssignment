0fa2513ddfb6a1d7a45a0ce0e7333de3883c268b
Poompavai <poompavaisaravanan@gmail.com>
2021-09-11 17:10:32 +0530

    Initial commit

---
+++ Jenkins/src/test/java/Practise/Jenkins/testcase.java
@@ -0,0 +1,19 @@
+package Practise.Jenkins;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testcase {
+	WebDriver driver;
+	@Test
+	public void InvokeBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().window().maximize();
+	}
+
+}
